39fac155b4fe8c0ed48d2e75f90cd8209e2ab5c5
jcpaeztello <141853470+jcpaeztello@users.noreply.github.com>
2026-05-08 15:05:17 -0500

    arreglado

--- src/routers/telemetry.ts
+++ src/routers/telemetry.ts
@@ -1,12 +1,14 @@
-import { Request, Response, Application } from "express";
-import { createTelemetry, getAllTelemetry } from "../controllers/telemetry.controller";
+import { Application } from "express";
+import { TelemetryController } from "../controllers/telemetry.controller";
 
 export class TelemetryRoutes {
 
+    public telemetryController: TelemetryController = new TelemetryController();
+
     public routes(app: Application): void {
 
-        app.route("/api/solar/telemetry").post(createTelemetry);
-        app.route("/api/solar/telemetry").get(getAllTelemetry);
+        app.route("/api/solar/telemetry").post(this.telemetryController.create);
+        app.route("/api/solar/telemetry").get(this.telemetryController.getAll);
 
     }
 
